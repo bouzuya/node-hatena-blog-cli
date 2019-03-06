ed8348b2c91446deb7ff551af28f00a0830c8a59
bouzuya <m@bouzuya.net>
2019-03-06 19:14:19 +0900

    add foreign

---
+++ src/Main.js
@@ -0,0 +1,45 @@
+exports.newClient = function (params) {
+  return function () {
+    return new (require('hatena-blog-api').Client)(params);
+  };
+};
+
+exports.create = function (params) {
+  return function (client) {
+    return function () {
+      return client.create(params);
+    };
+  };
+};
+
+exports.delete = function (memberUrl) {
+  return function (client) {
+    return function () {
+      return client.delete(memberUrl);
+    };
+  };
+};
+
+exports.edit = function (memberUrl) {
+  return function (params) {
+    return function (client) {
+      return function () {
+        return client.edit(memberUrl, params);
+      };
+    };
+  };
+};
+
+exports.list = function (client) {
+  return function () {
+    return client.list();
+  };
+};
+
+exports.retrieve = function (memberUrl) {
+  return function (client) {
+    return function () {
+      return client.retrieve(memberUrl);
+    };
+  };
+};
