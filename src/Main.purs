ba676ad00c6552b4d4b90bc1e08c17a50540fcaf
bouzuya <m@bouzuya.net>
2019-03-09 14:33:31 +0900

    add global option (help)

--- src/Main.purs
+++ src/Main.purs
@@ -14,7 +14,7 @@ import Command.Show as CommandShow
 import Command.Update as CommandUpdate
 import Data.Array as Array
 import Data.Either as Either
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Maybe as Maybe
 import Effect (Effect)
 import Effect.Aff as Aff
@@ -42,28 +42,55 @@ loadConfig = do
 formatEntry :: Entry -> String
 formatEntry { published, title } = published <> " " <> title
 
+help :: String
+help =
+  Array.intercalate
+    "\n"
+    [ "Usage: hatena-blog [options] <command>"
+    , ""
+    , "Commands:"
+    , "  create  create an entry"
+    , "  destroy destroy an entry"
+    , "  index   list entries"
+    , "  show    show an entry"
+    , "  update  update an entry"
+    , ""
+    , "Configs:"
+    , "  HATENA_API_KEY your hatena api key. e.g. xxxxxxxxxx"
+    , "  HATENA_BLOG_ID your hatena blog id. e.g. bouzuya.hatenablog.com"
+    , "  HATENA_ID      your hatena id.      e.g. bouzuya"
+    , ""
+    , "Options:"
+    , "  -h, --help show help"
+    ]
+
 main :: Effect Unit
 main = do
-  configMaybe <- loadConfig
-  config <- Maybe.maybe (Exception.throw "invalid config") pure configMaybe
-  client <- Client.newClient (Record.merge config { authType: "wsse" })
   args <- map (Array.drop 2) Process.argv
-  { arguments } <-
+  { arguments, options } <-
     Either.either
       (const (Exception.throw "invalid option"))
       pure
-      -- TODO: global options
-      (CommandLineOption.parseWithOptions { greedyArguments: true } {} args)
-  command <-
-    Maybe.maybe
-      (Exception.throw "no command")
-      pure
-      (Array.index arguments 0)
-  let commandArgs = Array.drop 1 arguments
-  case command of
-    "index" -> Aff.launchAff_ (CommandIndex.command client commandArgs)
-    "create" -> Aff.launchAff_ (CommandCreate.command client commandArgs)
-    "show" -> Aff.launchAff_ (CommandShow.command client commandArgs)
-    "update" -> Aff.launchAff_ (CommandUpdate.command client commandArgs)
-    "destroy" -> Aff.launchAff_ (CommandDestroy.command client commandArgs)
-    _ -> Console.log "unknown command"
+      (CommandLineOption.parseWithOptions
+        { greedyArguments: true }
+        { help: CommandLineOption.booleanOption "help" (Just 'h') "show help" }
+        args)
+  if options.help
+    then Console.log help
+    else do
+      command <-
+        Maybe.maybe
+          (Exception.throw "no command")
+          pure
+          (Array.index arguments 0)
+      let commandArgs = Array.drop 1 arguments
+      configMaybe <- loadConfig
+      config <- Maybe.maybe (Exception.throw "invalid config") pure configMaybe
+      client <- Client.newClient (Record.merge config { authType: "wsse" })
+      case command of
+        "create" -> Aff.launchAff_ (CommandCreate.command client commandArgs)
+        "destroy" -> Aff.launchAff_ (CommandDestroy.command client commandArgs)
+        "index" -> Aff.launchAff_ (CommandIndex.command client commandArgs)
+        "show" -> Aff.launchAff_ (CommandShow.command client commandArgs)
+        "update" -> Aff.launchAff_ (CommandUpdate.command client commandArgs)
+        _ -> Exception.throw "unknown command"
